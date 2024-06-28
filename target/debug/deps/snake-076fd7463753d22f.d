58b77232c3ef99f2872ae93cc3cef210a955cc3f
amroumaj <amrou.majdoub@etudiant-e>
2024-06-28 13:06:28 +0100

    added the game.rs and worked on snake.rs

--- target/debug/deps/snake-076fd7463753d22f.d
+++ target/debug/deps/snake-076fd7463753d22f.d
@@ -1,7 +1,8 @@
-/home/amrou/Documents/Programming/rust/snake/target/debug/deps/libsnake-076fd7463753d22f.rmeta: src/main.rs src/draw.rs src/snake.rs
+/home/amrou/Documents/Programming/rust/snake/target/debug/deps/libsnake-076fd7463753d22f.rmeta: src/main.rs src/draw.rs src/snake.rs src/game.rs
 
-/home/amrou/Documents/Programming/rust/snake/target/debug/deps/snake-076fd7463753d22f.d: src/main.rs src/draw.rs src/snake.rs
+/home/amrou/Documents/Programming/rust/snake/target/debug/deps/snake-076fd7463753d22f.d: src/main.rs src/draw.rs src/snake.rs src/game.rs
 
 src/main.rs:
 src/draw.rs:
 src/snake.rs:
+src/game.rs:
--- target/debug/deps/snake-7191cea51ca9b3ca.d
+++ target/debug/deps/snake-7191cea51ca9b3ca.d
@@ -1,7 +1,8 @@
-/home/amrou/Documents/Programming/rust/snake/target/debug/deps/libsnake-7191cea51ca9b3ca.rmeta: src/main.rs src/draw.rs src/snake.rs
+/home/amrou/Documents/Programming/rust/snake/target/debug/deps/libsnake-7191cea51ca9b3ca.rmeta: src/main.rs src/draw.rs src/snake.rs src/game.rs
 
-/home/amrou/Documents/Programming/rust/snake/target/debug/deps/snake-7191cea51ca9b3ca.d: src/main.rs src/draw.rs src/snake.rs
+/home/amrou/Documents/Programming/rust/snake/target/debug/deps/snake-7191cea51ca9b3ca.d: src/main.rs src/draw.rs src/snake.rs src/game.rs
 
 src/main.rs:
 src/draw.rs:
 src/snake.rs:
+src/game.rs:
